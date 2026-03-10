be7f1f503a63398588071fa45ffd5d71d284db77
Ciel <MrNoerGlugger@gmail.com>
2026-03-10 20:03:28 +0100

    x_min & y_min are now actually used

--- gpu/specialized_drawables/Screen.cpp
+++ gpu/specialized_drawables/Screen.cpp
@@ -42,13 +42,13 @@ SDL_FRect Screen::get_bounds() {
     bool start_value_set = false;
     for (auto drawable_pair : drawable_default_map) {
         auto drawable = drawable_pair.second;
-        if (!start_value_set || x_min > drawable->get_bounds().x) {
+        if (x_min > drawable->get_bounds().x) {
             x_min = drawable->get_bounds().x;
         }
         if (!start_value_set || x_max < drawable->get_bounds().x + drawable->get_bounds().w) {
             x_max = drawable->get_bounds().x + drawable->get_bounds().w;
         }
-        if (!start_value_set || y_min > drawable->get_bounds().y) {
+        if (y_min > drawable->get_bounds().y) {
             y_min = drawable->get_bounds().y;
         }
         if (!start_value_set || y_max < drawable->get_bounds().y + drawable->get_bounds().h) {
